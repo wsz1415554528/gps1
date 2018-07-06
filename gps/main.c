758b4e98e262dc9193f5502161f81078d82c5c2c
s20161104602 <s20161104602@ciec-imacs-imac.local>
2018-07-06 17:02:02 +0800

    Initial Commit

---
+++ gps/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  gps
+//
+//  Created by s20161104602 on 2018/7/6.
+//  Copyright © 2018年 s20161104602. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
